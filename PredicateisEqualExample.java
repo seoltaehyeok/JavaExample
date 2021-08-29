423482b0fbd0bf0f34d3e4210dad29a5296436a2
seoltaehyeok <83220871+seoltaehyeok@users.noreply.github.com>
2021-08-29 23:31:59 +0900

    Ramda_isEquall()
    isEquall() 정적 메소드
    
    Predicate<Object> predicate = PredicateisEqual(targetObject);
    boolean result = predicate.test(sourceObject);
    
    sourceObject / targetObject
    둘다 null 일때 true 리턴
    둘다 not null 일때 sourceObject.equals(targetObject)의 리턴 값
    나머지는 false를 리턴 (하나는 null 하나는 notnull인 경우)

---
+++ PredicateisEqualExample.java
@@ -0,0 +1,23 @@
+package Ramda_isEquals;
+
+import java.util.function.Predicate;
+
+public class PredicateisEqualExample {
+
+	public static void main(String[] args) {
+		Predicate<String> predicate;
+		
+		predicate = Predicate.isEqual(null);
+		System.out.println("null, null: " + predicate.test(null));
+		
+		predicate = Predicate.isEqual("Java8");
+		System.out.println("null, Java8: " + predicate.test(null));
+		
+		predicate = Predicate.isEqual("null");
+		System.out.println("null, Java8: " + predicate.test("Java8"));
+		
+		predicate = Predicate.isEqual("Java8");
+		System.out.println("Java8, Java8: " + predicate.test("Java8"));
+	}
+
+}
