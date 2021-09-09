01318252f29b5c7f7092fd56dbcc22a22425a3cb
seoltaehyeok <83220871+seoltaehyeok@users.noreply.github.com>
2021-09-09 23:52:04 +0900

    Collection Framework ( setCollection )
    [set컬렉션]
    수학의 집합에 비유될 수 있다.
    저장 순서가 유지되지 않는다.
    객체를 중복해서 저장할 수 없다.
    하나의 null만 저장할 수 있다.
    
    구현클래스 : 
    HashSet, LinkedHashSet, TreeSet
    
    Set컬렉션은 인덱스로 객체를 검색해서 가져오는 메소드가 없다.
    전체 객체를 대상으로 한번씩 반복해서 가져오는 반복자(Iterator)를 제공한다.
    
    HashSet
    Set<E> set = new HashSet<E>();
    동일 객체 및 동등 객체는 중복 저장하지 않는다.
    두 객체의 hashCode()의 리턴값 비교=>
    값이 같은 경우: equals()의 리턴값을 비교 true인 경우
    동일객체: 참조하는 객체가 같음
    동등객체: 참조하는 객체는 다르나 객체의 값은 같음
    
    hasNext() : set컬렉션에서 가져올 객체가 있으면 true를 반환 아니면 false 반환
    
    -------------------------------------------------------
    출력: 
    총 객체수: 4
    	Java
    	JDBC
    	Servlet/JSP
    	iBATIS
    총 객체수: 2
    	Java
    	Servlet/JSP
    비어 있음

---
+++ HashSetExample1.java
@@ -0,0 +1,42 @@
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample1 {
+
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		
+		set.add("Java");
+		set.add("JDBC");
+		set.add("Servlet/JSP");
+		set.add("Java"); //set의 경우 중복된 데이터를 저장할 수 없다.
+		set.add("iBATIS");
+		
+		int size = set.size();
+		System.out.println("총 객체수: " + size);
+		
+		Iterator<String> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String element = iterator.next();
+			System.out.println("\t" + element);
+		}
+		
+		set.remove("JDBC");
+		set.remove("iBATIS");
+		
+		
+		System.out.println("총 객체수: " + set.size());
+		
+		for(String element : set) {
+			System.out.println("\t" + element);
+		}
+		
+		set.clear();
+		if(set.isEmpty()) { // set컬렉션이 비어있으면 true리턴
+			System.out.println("비어 있음");
+		}
+	}
+}
