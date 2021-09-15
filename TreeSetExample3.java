3544aa49a72f8d128f1cbd8aacca9b4b9e71c964
seoltaehyeok <83220871+seoltaehyeok@users.noreply.github.com>
2021-09-15 22:08:46 +0900

    Collection Framework ( TreeSet ) (3)
    treeSet.subSet(fromElement, fromInclusive, toElement, toInclusive);
    treeSet.subSet(시작 값, 포함여부T/F, 끝 값, 포함여부T/F);
    
    -----------------------------------
    
    출력: 
    [c~f 사이의 단어 검색]
    cherry
    description
    ever

---
+++ TreeSetExample3.java
@@ -0,0 +1,26 @@
+package treeSet;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetExample3 {
+
+	public static void main(String[] args) {
+		TreeSet<String> treeSet = new TreeSet<String>();
+		treeSet.add("apple");
+		treeSet.add("forever");
+		treeSet.add("description");
+		treeSet.add("ever");
+		treeSet.add("zoo");
+		treeSet.add("base");
+		treeSet.add("guess");
+		treeSet.add("cherry");
+		
+		System.out.println("[c~f 사이의 단어 검색]");
+		NavigableSet<String> rangeSet = treeSet.subSet("c", true, "f", true); // 범위 지정시 사용 true인 경우 그 문자도 포함
+		for(String word : rangeSet) {
+			System.out.println(word);
+		}
+	}
+
+}
